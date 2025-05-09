65a58fb22231d9e318990eb418a7fc2580ce5ee8
JahdielArciniegas <arciniegasjahdiel@gmail.com>
2025-05-09 12:09:54 -0400

    [PRO/CONT] feat: Integrando Clerk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
+import { esES } from "@clerk/localizations";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [
+    clerk({
+      localization: esES,
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
+  adapter: node({ mode: "standalone" }),
+  output: "server",
 });
