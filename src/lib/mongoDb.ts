e97c0ea518008110b5a7d9dbaf92190d3ece00cb
Jahdiel Arciniegas <arciniegasjahdiel@gmail.com>
2026-04-03 10:59:35 -0400

    refactor: cambiando minimamente el documento para conectarse con mongoose

--- src/lib/mongoDb.ts
+++ src/lib/mongoDb.ts
@@ -5,10 +5,7 @@ const uri = import.meta.env.MONGO_URI;
 export async function connectToDB() {
   if (mongoose.connection.readyState >= 1) return;
   try {
-    await mongoose.connect(uri, {
-      dbName: "procont",
-    });
-    console.log("Connected to MongoDB");
+    await mongoose.connect(uri);
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
   }
