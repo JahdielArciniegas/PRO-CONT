f4671466e753d570090695392c2b43e607731e40
Jahdiel Arciniegas <arciniegasjahdiel@gmail.com>
2026-04-20 09:47:06 -0400

    [PRO/CONT] feat: implementando señal de carga cuando se quieren obtener las tablas

--- src/components/Boards.tsx
+++ src/components/Boards.tsx
@@ -2,7 +2,7 @@ import type { Board } from "@src/lib/types";
 import { useEffect, useState, Suspense } from "react";
 import { Card, CardHeader, CardTitle } from "./ui/card";
 import { Button } from "./ui/button";
-import { Trash, ArrowLeft, ArrowRight } from "lucide-react";
+import { Trash, ArrowLeft, ArrowRight, Loader2 } from "lucide-react";
 import { useNotificationStore } from "@src/lib/store";
 import { deleteBoard, getBoards } from "@src/service/boards";
 
@@ -10,10 +10,12 @@ const PAGE_SIZE = 4;
 
 const Boards = ({ userId }: { userId: string }) => {
   const [boards, setBoard] = useState<Board[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
   const [currentPage, setCurrentPage] = useState(1);
   const { addNotification } = useNotificationStore();
   useEffect(() => {
     const boards = async () => {
+      setLoading(true);
       try {
         const board = await getBoards(userId);
         setBoard(board);
@@ -26,6 +28,8 @@ const Boards = ({ userId }: { userId: string }) => {
           message: "Error al cargar las tablas",
           type: "error",
         });
+      } finally {
+        setLoading(false);
       }
     };
     boards();
@@ -58,71 +62,78 @@ const Boards = ({ userId }: { userId: string }) => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <div className="relative flex flex-col gap-4 w-full">
-        <ul className="p-4 pt-12 grid sm:grid-cols-2 grid-cols-1 grid-row-2 sm:gap-4 gap-2 max-sm:h-96 max-sm:overflow-y-auto no-scrollbar">
-          {paginatedBoards.map((board) => (
-            <li key={board.id} className="flex justify-center items-center">
-              <Card className="sm:h-80 h-40 sm:w-96 w-full">
-                <CardHeader className="">
-                  <CardTitle className="lg:text-3xl h-12 text-xl flex gap-2 items-center justify-center text-center">
-                    <a
-                      className="hover:text-muted-foreground transition-colors duration-300"
-                      href={`/board/${board.id}`}
-                    >
-                      {board.title}
-                    </a>
-                    <Button
-                      onClick={() => handleDelete(board.id)}
-                      variant="destructive"
-                      className="cursor-pointer"
-                    >
-                      <Trash />
-                    </Button>
-                  </CardTitle>
-                </CardHeader>
-                <div className="flex sm:gap-4 gap-2">
-                  <div className="flex flex-col gap-2 w-1/2 sm:h-52 h-24 sm:p-4 p-2 ">
-                    <h4 className="sm:text-2xl text-lg w-20 sm:w-auto mx-auto sm:m-0 font-bold text-center bg-green-500 rounded-lg">
-                      Pros
-                    </h4>
-                    <ul className="hidden sm:flex flex-col gap-2 sm:h-52 h-32 p-4 overflow-y-auto  no-scrollbar">
-                      {board.pros.map((pros: string) => (
-                        <li
-                          key={Math.random()}
-                          className="text-sm sm:text-base"
-                        >
-                          {pros}
-                        </li>
-                      ))}
-                    </ul>
+        {!loading ? (
+          <ul className="p-4 pt-12 grid sm:grid-cols-2 grid-cols-1 grid-row-2 sm:gap-4 gap-2 max-sm:h-96 max-sm:overflow-y-auto no-scrollbar">
+            {paginatedBoards.map((board) => (
+              <li key={board.id} className="flex justify-center items-center">
+                <Card className="sm:h-80 h-40 sm:w-96 w-full">
+                  <CardHeader className="">
+                    <CardTitle className="lg:text-3xl h-12 text-xl flex gap-2 items-center justify-center text-center">
+                      <a
+                        className="hover:text-muted-foreground transition-colors duration-300"
+                        href={`/board/${board.id}`}
+                      >
+                        {board.title}
+                      </a>
+                      <Button
+                        onClick={() => handleDelete(board.id)}
+                        variant="destructive"
+                        className="cursor-pointer"
+                      >
+                        <Trash />
+                      </Button>
+                    </CardTitle>
+                  </CardHeader>
+                  <div className="flex sm:gap-4 gap-2">
+                    <div className="flex flex-col gap-2 w-1/2 sm:h-52 h-24 sm:p-4 p-2 ">
+                      <h4 className="sm:text-2xl text-lg w-20 sm:w-auto mx-auto sm:m-0 font-bold text-center bg-green-500 rounded-lg">
+                        Pros
+                      </h4>
+                      <ul className="hidden sm:flex flex-col gap-2 sm:h-52 h-32 p-4 overflow-y-auto  no-scrollbar">
+                        {board.pros.map((pros: string) => (
+                          <li
+                            key={Math.random()}
+                            className="text-sm sm:text-base"
+                          >
+                            {pros}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                    <span className="w-0.5 sm:h-52 h-12 bg-foreground"></span>
+                    <div className="flex flex-col gap-2 w-1/2 sm:h-52 h-24 sm:p-4 p-2">
+                      <h4 className="sm:text-2xl text-lg w-20 sm:w-auto mx-auto sm:m-0  font-bold text-center bg-red-500 rounded-lg">
+                        Contras
+                      </h4>
+                      <ul className="hidden sm:flex flex-col gap-2 h-52 p-4 overflow-y-auto no-scrollbar">
+                        {board.cons.map((cons: string) => (
+                          <li
+                            key={Math.random()}
+                            className="text-sm sm:text-base"
+                          >
+                            {cons}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
                   </div>
-                  <span className="w-0.5 sm:h-52 h-12 bg-foreground"></span>
-                  <div className="flex flex-col gap-2 w-1/2 sm:h-52 h-24 sm:p-4 p-2">
-                    <h4 className="sm:text-2xl text-lg w-20 sm:w-auto mx-auto sm:m-0  font-bold text-center bg-red-500 rounded-lg">
-                      Contras
-                    </h4>
-                    <ul className="hidden sm:flex flex-col gap-2 h-52 p-4 overflow-y-auto no-scrollbar">
-                      {board.cons.map((cons: string) => (
-                        <li
-                          key={Math.random()}
-                          className="text-sm sm:text-base"
-                        >
-                          {cons}
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
-              </Card>
-            </li>
-          ))}
-          {boards.length > 0 &&
-            Array.from({ length: 4 - boards.length }).map((_, index) => (
-              <li
-                key={index}
-                className="invisible h-24 sm:h-80 w-full sm:w-96"
-              ></li>
+                </Card>
+              </li>
             ))}
-        </ul>
+            {boards.length > 0 &&
+              Array.from({ length: 4 - boards.length }).map((_, index) => (
+                <li
+                  key={index}
+                  className="invisible h-24 sm:h-80 w-full sm:w-96"
+                ></li>
+              ))}
+          </ul>
+        ) : (
+          <div className="flex justify-center items-center h-96">
+            <Loader2 className="animate-spin" />
+            <p className="text-center">Cargando...</p>
+          </div>
+        )}
         {boards.length === 0 && <p className="text-center">No hay tableros</p>}
 
         <div className="flex gap-4 -mt-4 justify-center w-full z-10">
